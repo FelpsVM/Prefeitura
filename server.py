68159f9cd0c170e55fd78d93851f01a2d8f3150c
FelpsVM <femariano77@outlook.com>
2026-08-22 11:05:31 -0300

    Init of data

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,25 @@ def register():
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route("/createUser", methods=["POST"])
+def createUser():
+    dados = request.get_json()
+
+    name = dados["name"]
+    email = dados["email"]
+    number = dados["number"]
+    password = dados["password"]
+
+    print(name)
+    print(email)
+    print(number)
+    print(password)
+
+    return jsonify({
+    "success": True,
+    "message": "Usuário criado!"
+})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/register.js
+++ static/register.js
@@ -1,5 +1,34 @@
-var btn = document.getElementById("registerBTN")
+var form = document.querySelector("form");
 
-btn.addEventListener("click", function() {
-    console.log("Registrando");
-})
+function register(name, email, number, pass) {
+    fetch("/createUser", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            number: number,
+            password: pass
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error("Erro:", error);
+    });
+}
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var number = document.getElementById("phone").value;
+    var password = document.getElementById("password").value;
+
+    register(name, email, number, password);
+});
